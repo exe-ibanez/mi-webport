75dd4c7f42bc165e5456dfb8c9ef53c6de3037bf
Exe Ibañez <peyielperro@gmail.com>
2023-09-29 23:07:00 -0300

    Hide and show the navbar menu

---
+++ src/script/index.js
@@ -0,0 +1,5 @@
+let menu = document.getElementById('menu-navbar');
+let BtonMenuToggle = document.getElementById('switch-menu');
+BtonMenuToggle.addEventListener('click', function (){
+  menu.classList.toggle('show-hide');
+})
