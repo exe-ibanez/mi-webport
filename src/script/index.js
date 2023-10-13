264b71f0c1519f0835eb17da6ee7b9e9e1d8986d
Exe Ibañez <peyielperro@gmail.com>
2023-10-13 18:45:35 -0300

    Restarting all

--- src/script/index.js
+++
@@ -1,5 +0,0 @@
-let menu = document.getElementById('menu-navbar');
-let BtonMenuToggle = document.getElementById('switch-menu');
-BtonMenuToggle.addEventListener('click', function (){
-  menu.classList.toggle('show-hide');
-})
